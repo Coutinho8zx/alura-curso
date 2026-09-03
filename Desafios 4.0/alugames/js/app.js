5cd5ef15becd8fcdc32c7d86ec6111b3181b0e50
Coutinho8zx <coutinho20110@gmail.com>
2026-09-03 11:08:02 -0300

    Add files via upload

---
+++ Desafios 4.0/alugames/js/app.js
@@ -0,0 +1,34 @@
+let jogosAlugados = 0;
+
+function mostrarJogosAlugados(){
+    console.log(`Total de jogos alugados: ${jogosAlugados}`)
+}
+
+
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name');;
+
+    if (imagem.classList.contains('dashboard__item__img--rented')) {
+      if(alert(`voce tem certeza o vai devolver o jogo ${nomeJogo.textContent}`));
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.textContent = 'Devolver';
+    }
+    mostrarJogosAlugados();
+}
+
+//outro desafio//
+ // function ordenarNumeros(a, b, c) {
+    //const numerosOrdenados = [a, b, c].sort((x, y) => x - y);
+    //console.log(`Números ordenados: ${numerosOrdenados.join(', ')}`);
+
+
+// Exemplo de uso:
+///ordenarNumeros(3, 1, 5);
