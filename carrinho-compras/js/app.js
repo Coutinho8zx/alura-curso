842ffcdb9716379ae9cd93f6ba95d1f682e46443
Coutinho8zx <coutinho20110@gmail.com>
2026-09-03 11:28:32 -0300

    Add files via upload

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,15 @@
+function adicionar() {
+    //recuperar valores nome do produto, quantidade e valor 
+    let nomeProduto = document.getElementById('produto'); 
+    let quantidade = document.getElementById('quantidade');
+    alert(nomeProduto.value);
+    alert(quantidade.value);
+
+    //calcular o preço, o nosso subtotal
+    //adicionar no carrinho
+    //atualizar valor total 
+}
+
+function limpar() {
+
+}
