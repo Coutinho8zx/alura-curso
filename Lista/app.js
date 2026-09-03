fdbb25bc1063758193362d034cd77bb9dc04ef19
Coutinho8zx <coutinho20110@gmail.com>
2026-09-03 08:32:15 -0300

    Add files via upload

---
+++ Lista/app.js
@@ -0,0 +1,8 @@
+document.getElementById("botao").addEventListener("click" , function(){
+    
+  if (lista.style.display === 'none' || lista.style.display === "") {
+    lista.style.display = 'block';
+  } else {
+    lista.style.display = 'none';
+  }
+  });
