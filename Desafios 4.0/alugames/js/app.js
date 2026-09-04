a7a3bb9702b7ddce30979e5ae1588a041fcfd469
Coutinho8zx <coutinho20110@gmail.com>
2026-09-04 09:26:54 -0300

    Add files via upload

--- Desafios 4.0/alugames/js/app.js
+++ Desafios 4.0/alugames/js/app.js
@@ -32,3 +32,35 @@ function alterarStatus(id) {
 
 // Exemplo de uso:
 ///ordenarNumeros(3, 1, 5);
+
+
+//calculadore//
+//function adicao(a,b){
+    //return a + b ;
+//} 
+//function subtracao(a,b){
+    //return a - b ;
+//}
+//function multiplicacao(a,b){
+    //return a * b;
+//} 
+//function divisao(a,b){
+  //if(b !== 0){
+    //return a / b;
+  //} else {
+   // return:"erro : divisao por zero";
+  //}
+//}
+
+//function calculadora(operacao, a, b) {
+   // switch (operacao) {
+      //  case 'soma':
+           // return adicao(a, b);
+        //case 'subtracao':
+           // return subtracao(a, b);
+        //case 'multiplicacao':
+            //return multiplicacao(a, b);
+       // case 'divisao':
+            //return divisao(a, b);
+        //default:
+           // return "Operação inválida.";
